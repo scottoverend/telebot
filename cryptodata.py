42e87d0518a6dbf172f2a333ab1064c6c9dea2da
Scott <122386478+scottoverend@users.noreply.github.com>
2024-03-15 02:16:24 +0000

    Update cryptodata.py

--- cryptodata.py
+++ cryptodata.py
@@ -1,9 +1,33 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from pycoingecko import CoinGeckoAPI
+
 cg = CoinGeckoAPI()
 
-cg.ping()
+async def send_price_btc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
+    price_data = cg.get_price(ids='bitcoin', vs_currencies='usd')
+    price = price_data['bitcoin']['usd']
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The price of bitcoin is ${price}")
 
-# /simple/price endpoint with the required parameters
-cg.get_price(ids='bitcoin', vs_currencies='usd')
-{'bitcoin': {'usd': 3462.04}}
+async def send_price_eth(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
+    price_data = cg.get_price(ids='ethereum', vs_currencies='usd')
+    price = price_data['ethereum']['usd']
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The price of ethereum is ${price}")
 
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('7082309788:AAHS2HHwLDjoeQE99zk68BUBr-RNcnVnw_Y').build()
+    
+    price_handler1 = CommandHandler('bitcoinprice', send_price_btc)
+    
+    price_handler2 = CommandHandler('ethereumprice', send_price_eth)
+    
+    price_handler3 = CommandHandler('solanaprice', send_price_eth)
+    
+    application.add_handler(price_handler1)
+    
+    application.add_handler(price_handler2)
+    
+    application.run_polling()
