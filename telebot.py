8f89e1955cc11827207ac3806b2dfd304d4afc26
Scott <122386478+scottoverend@users.noreply.github.com>
2024-04-04 16:11:39 +0100

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -6,23 +6,29 @@ from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
 async def send_price_btc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
     price_data = cg.get_price(ids='bitcoin', vs_currencies='usd')
     price = price_data['bitcoin']['usd']
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The price of bitcoin is ${price}")
 
 async def send_price_eth(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
     price_data = cg.get_price(ids='ethereum', vs_currencies='usd')
     price = price_data['ethereum']['usd']
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The price of ethereum is ${price}")
-
+    
 async def send_price_sol(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
     price_data = cg.get_price(ids='solana', vs_currencies='usd')
     price = price_data['solana']['usd']
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The price of solana is ${price}")
-
-##def send_welcome(message):
-        ##bot.reply_to(message, "Howdy")
-        
+    
+    
+async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f".................")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello!")
+    
+    
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7082309788:AAHS2HHwLDjoeQE99zk68BUBr-RNcnVnw_Y').build()
     
@@ -32,7 +38,7 @@ if __name__ == '__main__':
     
     price_handler3 = CommandHandler('solanaprice', send_price_sol)
     
-    ##message = CommandHandler('hello', send_welcome)
+    hello_handler4 = CommandHandler('hello', say_hello)
     
     application.add_handler(price_handler1)
     
@@ -40,5 +46,13 @@ if __name__ == '__main__':
     
     application.add_handler(price_handler3)
     
-
-##the website is: 
+    application.add_handler(hello_handler4)
+    
+    application.run_polling()
+    
+    
+# fairly deep explanation: https://github.com/ggerganov/llama.cpp#build 
+# https://vishnu.im/posts/run-your-own-ai-bot-with-llama-telegram-bot/
+# https://github.com/abetlen/llama-cpp-python?tab=readme-ov-file
+# https://github.com/ggerganov/llama.cpp#build
+# https://github.com/ggerganov/llama.cpp/tree/ac43576124a75c2de6e333ac31a3444ff9eb9458?tab=readme-ov-file#hot-topics
